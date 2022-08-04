bc04af92b7a49e0098379669d9e71e9230b37bb3
husanbek <husanbek4030m@gmail.com>
2022-08-04 18:26:48 +0500

    finishied two task

---
+++ Animations/screens/HomeScreen.swift
@@ -0,0 +1,52 @@
+//
+//  HomeScreen.swift
+//  Animations
+//
+//  Created by Karavella on 04/08/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    @State private var half = false
+    @State private var dim = false
+    
+    @State var play = 0
+    
+    var body: some View {
+        TabView {
+            VStack{
+                Image("tower")
+                    .aspectRatio(contentMode: .fit)
+                    .scaleEffect(half ? 0.5 : 1.0)
+                    .opacity(dim ? 0.5 : 1.0)
+                    .onTapGesture {
+                        self.half.toggle()
+                        
+                        withAnimation(.easeInOut(duration: 1.0)) {
+                            self.dim.toggle()
+                        }
+                    }
+            }.tabItem {
+                Image(systemName: "clock")
+                Text("Local animation")
+            }.tag(0)
+            
+            VStack{
+                LottieView(name: "face", play: $play)
+                    .onTapGesture {
+                        self.play += 1
+                    }
+            }.tabItem {
+                Image(systemName: "clock")
+                Text("Lotte animation")
+            }.tag(1)
+        }
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
